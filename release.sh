3857a1fad15a05539bc60567d25c83a4caa8a666
李铭昕 <715557344@qq.com>
2018-03-10 08:38:52 +0800

    Fix release.sh

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # 暂时无法使用
-ssh -i /var/jenkins_home/.ssh/id_rsa -t -t root@172.17.0.1
+ssh -i /var/jenkins_home/.ssh/id_rsa -tt root@172.17.0.1
 cd /www/limx/phalcon
 git pull && \
 composer update --no-dev --prefer-dist -o && \
